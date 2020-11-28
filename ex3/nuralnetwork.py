2d8dcddfa3ede29476eca05110d554a3146ade6e
satoshi <hase.sato.8888@gmail.com>
2020-11-28 12:39:05 +0900

    forward

--- ex3/nuralnetwork.py
+++ ex3/nuralnetwork.py
@@ -6,23 +6,29 @@ def sigmoid(x):
 def identity_function(x):
     return x
 
-X = np.array([1.0,0.5])
-W1 = np.array([[0.1,0.3,0.5],[0.2,0.4,0.6]])
-B1 = np.array([0.1,0.2,0.3])
+def init_network():
+    network = {}
+    network['w1'] = np.array([[0.1,0.3,0.5],[0.2,0.4,0.6]])
+    network['b1'] = np.array([0.1,0.2,0.3])
+    network['w2'] = np.array([[0.1,0.4],[0.2,0.5],[0.3,0.6]])
+    network['b2'] = np.array([0.1,0.2])
+    network['w3'] = np.array([[0.1,0.3],[0.2,0.4]])
+    network['b3'] = np.array([0.1,0.2])
+    return network
 
-W2 = np.array([0.1,0.4],[0.2,0.5],[0.3,0.6])
-B2 = np.array([0.1,0.2])
+def forward(network,x):
+    w1,w2,w3 = network['w1'],network['w2'],network['w3']
+    b1,b2,b3 = network['b1'],network['b2'],network['b3']
+    a1 = np.dot(x,w1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1,w2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2,w3) + b3
+    y = identity_function(a3)
+    return y
 
-W3 = np.array([[0.1,0.3],[0.2,0.4]])
-B3 = np.array([0.1,0.2])
-
-
-A1 = np.dot(X,W1) + B1
-Z1 = sigmoid(A1)
-
-A2 = np.dot(Z1,W2) + B2
-Z2 = sigmoid(A2)
-
-A3 = np.dot(Z2,W3) + B3
-
-Y = identity_function(A3)
+if __name__ == "__main__":
+    network = init_network()
+    x = np.array([1.0,0.5])
+    y = forward(network,x)
+    print(y)
