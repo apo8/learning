67551e29ebba55f2b1b8806b62ba63f2866d7bd8
satoshi <hase.sato.8888@gmail.com>
2020-12-02 19:17:56 +0900

    convolution

--- ex7/convolution.py
+++ ex7/convolution.py
@@ -24,6 +24,28 @@ class Convolution:
 
         return out
 
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride=1, pad=0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad = pad
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        out_h = int(1 + (H - self.pool_h) / self.stride)
+        out_w = int(1 + (W - self.pool_w) / self.stride)
+
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
+        col = col.reshape(-1, self.pool_h*self.pool_w)
+
+        #max
+        out = np.max(col, axis=1)
+
+        out - out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+
+        return out
+
 if __name__ == "__main__":
     x1 = np.random.rand(1,3,7,7)
     col1 = im2col(x1, 5, 5, stride=1, pad=0)
